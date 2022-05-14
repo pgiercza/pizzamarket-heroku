81176f626caa0d87965738a7867bdc83f6fa555d
Sebastian Słomian <slomian@wszib.edu.pl>
2022-05-14 09:47:07 +0200

    Inicjalizacja projektu

---
+++ src/main/java/pl/wszib/pizzamarket/PizzamarketApplication.java
@@ -0,0 +1,13 @@
+package pl.wszib.pizzamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzamarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzamarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wszib/pizzamarket/PizzamarketApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wszib.pizzamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzamarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
