5e42bb62f61d7e49e1b2cba3818b586ec6d6830e
Sebastian Słomian <slomian@wszib.edu.pl>
2022-05-14 16:07:17 +0200

    Strona zamawiania pizzy

--- src/main/java/pl/wszib/pizzamarket/web/controllers/MenuController.java
+++ src/main/java/pl/wszib/pizzamarket/web/controllers/MenuController.java
@@ -3,11 +3,16 @@ package pl.wszib.pizzamarket.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.wszib.pizzamarket.data.entities.PizzaEntity;
 import pl.wszib.pizzamarket.data.repositories.PizzaRepository;
+import pl.wszib.pizzamarket.web.models.OrderAddressModel;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("menu")
@@ -24,5 +29,15 @@ public class MenuController {
         List<PizzaEntity> pizzas = pizzaRepository.findAll();
         model.addAttribute("pizzas", pizzas);
         return "menuPage";
+    }
+
+    @GetMapping("order/{pizzaId}")
+    public String showPizzaOrderPage(
+            @PathVariable Long pizzaId,
+            @ModelAttribute("orderAddress") OrderAddressModel orderAddress,
+            Model model) {
+        PizzaEntity pizza = pizzaRepository.findById(pizzaId).orElseThrow(EntityNotFoundException::new);
+        model.addAttribute("pizza", pizza);
+        return "orderPizzaPage";
     }
 }
